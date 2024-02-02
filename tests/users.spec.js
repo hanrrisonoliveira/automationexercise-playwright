d00e79719416150a70065db4e9f3b76ac6cc4b2e
Hanrrison Oliveira <hanrrison.dos@gmail.com>
2024-02-02 13:58:10 -0300

    Installing fakerjs and code refatoring

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,10 +1,23 @@
 // @ts-check
+import { faker, fakerEN_US } from '@faker-js/faker';
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require ('../pages/LoginPage');
 const { SignupPage } = require ('../pages/SignupPage');
 const { HomePage } = require ('../pages/HomePage');
 const { AccountCreatedPage } = require('../pages/AccountCreatedPage');
 const { DeleteAccountPage } = require('../pages/DeleteAccountPage');
+
+const firstName = faker.person.firstName()
+const lastName = faker.person.lastName()
+const email = faker.internet.email({ firstName: firstName })
+const password = faker.internet.password({ length: 10 })
+const month = faker.date.month()
+const company = faker.company.name()
+const street = faker.location.street()
+const state = fakerEN_US.location.state()
+const city = fakerEN_US.location.city()
+const zipcode = fakerEN_US.location.zipCode()
+const mobileNumber = faker.phone.number()
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
@@ -16,35 +29,33 @@ test('Test Case 1: Register User', async ({ page }) => {
   const signupPage = new SignupPage(page)
   const accountCreatedPage = new AccountCreatedPage(page)
   const deleteAccountPage = new DeleteAccountPage(page)
- 
-  //Validar o titulo da pagina de uma boa forma
-  await expect(page.getByText('Home')).toBeVisible()
 
+  await expect(page).toHaveTitle('Automation Exercise');
   await homePage.signupLogin()
-  await loginPage.newUserSignup('Usuario Demonstracao', 'email@playwright.com')
+  await loginPage.newUserSignup(firstName, email)
   await expect(page.getByText('Enter Account Information')).toBeVisible()
   await signupPage.checkTitle()
-  await signupPage.fillPassword('secretpassword')
-  await signupPage.fillDay('31')
-  await signupPage.fillMonth('8')
+  await signupPage.fillPassword(password)
+  await signupPage.fillDay('5') //Pode ter um problema se cair dia 31 de fevereiro
+  await signupPage.fillMonth(month)
   await signupPage.fillYear('1993')
   await signupPage.checkNewsletter()
   await signupPage.checkSpecialOffers()
-  await signupPage.fillFirstName('FIRST NAME')
-  await signupPage.fillLastName('LAST NAME')
-  await signupPage.fillCompany('COMPANY')
-  await signupPage.fillAddress('ADDRESS ONE');
-  await signupPage.fillAddressTwo('ADDRESS TWO');
+  await signupPage.fillFirstName(firstName)
+  await signupPage.fillLastName(lastName)
+  await signupPage.fillCompany(company)
+  await signupPage.fillAddress(street);
+  await signupPage.fillAddressTwo('Second Address');
   await signupPage.fillCountry('United States')
-  await signupPage.fillState('UTAH')
-  await signupPage.fillCity('SALT LAKE CITY')
-  await signupPage.fillZipcode('13231231')
-  await signupPage.fillMobileNumber('5598958492')
+  await signupPage.fillState(state)
+  await signupPage.fillCity(city)
+  await signupPage.fillZipcode(zipcode)
+  await signupPage.fillMobileNumber(mobileNumber)
   await signupPage.createAccount()
   await expect(page.getByText('Account Created!')).toBeVisible()
   await accountCreatedPage.continue()
 
-  await expect(page.getByText('Logged in as')).toBeVisible()
+  await expect(page.getByText('Logged in as ' + firstName)).toBeVisible()
   await homePage.deleteAccount()
   await deleteAccountPage.continue()
 
